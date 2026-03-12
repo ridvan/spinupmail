338f907216016f0682cc936104288dfd5a004a4b
Rıdvan <63660173+ridvan@users.noreply.github.com>
2026-03-12 23:10:58 +0300

    chore: increase test timeout to 60s

--- packages/e2e/playwright.config.ts
+++ packages/e2e/playwright.config.ts
@@ -23,7 +23,7 @@ export default defineConfig({
   retries: process.env.CI ? 1 : 0,
   workers: process.env.CI ? 4 : undefined,
   reporter: [["list"], ["html", { open: "never" }]],
-  timeout: 30_000,
+  timeout: 60_000,
   expect: {
     timeout: 10_000,
   },
