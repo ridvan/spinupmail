c81f14eb3b1b9177be8dfb451581695ad94748f0
Rıdvan <63660173+ridvan@users.noreply.github.com>
2026-03-12 22:42:26 +0300

    4 parallel

--- packages/e2e/playwright.config.ts
+++ packages/e2e/playwright.config.ts
@@ -21,7 +21,7 @@ export default defineConfig({
   fullyParallel: false,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 1 : 0,
-  workers: process.env.CI ? 2 : undefined,
+  workers: process.env.CI ? 4 : undefined,
   reporter: [["list"], ["html", { open: "never" }]],
   timeout: 30_000,
   expect: {
