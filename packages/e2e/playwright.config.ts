127c3ccfff4d721f58dabdb2caff2bd9e644ecfc
Rıdvan <63660173+ridvan@users.noreply.github.com>
2026-03-12 23:22:17 +0300

    2 workers on ci

--- packages/e2e/playwright.config.ts
+++ packages/e2e/playwright.config.ts
@@ -21,7 +21,7 @@ export default defineConfig({
   fullyParallel: false,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 1 : 0,
-  workers: process.env.CI ? 4 : undefined,
+  workers: process.env.CI ? 2 : undefined,
   reporter: [["list"], ["html", { open: "never" }]],
   timeout: 60_000,
   expect: {
